6ab312d96bc499747f1c696e81d624f8ea57b4ed
Rodrigo Bermudez Schettino <13469-r.bermudezschettin@users.noreply.gitlab.gwdg.de>
2024-10-22 15:02:52 +0200

    Script: Remove mentions of nvm in start script

--- secret-hitler/on_start.bash
+++ secret-hitler/on_start.bash
@@ -1,9 +1,5 @@
 #!/bin/bash
-export NVM_DIR=$HOME/.nvm
-[ -s $NVM_DIR/nvm.sh ] && . $NVM_DIR/nvm.sh
-[ -s $NVM_DIR/bash_completion ] && . $NVM_DIR/bash_completion
-nvm use 16.13.0
-yarn prod&
+yarn prod &
 sleep 3
 yarn create-accounts
 sleep 10000000
