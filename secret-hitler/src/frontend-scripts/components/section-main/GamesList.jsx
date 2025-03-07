1b36c917dfdd1cd2d5e8f6c003c8a0e96a5b479a
Ali Dasmeh <dasmeh@mpib-berlin.mpg.de>
2025-03-07 09:29:40 +0100

    automatically remove player from queue when player close window

--- secret-hitler/src/frontend-scripts/components/section-main/GamesList.jsx
+++ secret-hitler/src/frontend-scripts/components/section-main/GamesList.jsx
@@ -227,6 +227,21 @@ const GamesList = (props)=> {
 		}
 	}, [inQueue])
 
+	const useUnload = (callback) => {
+		useEffect(() => {
+		  const handleUnload = (event) => {
+			callback();
+			event.preventDefault();
+			// event.returnValue = ""; // Some browsers require this for the prompt to show
+		  };
+	  
+		  window.addEventListener("beforeunload", handleUnload);
+		  return () => window.removeEventListener("beforeunload", handleUnload);
+		}, [callback]);
+	};
+
+	useUnload(() => { updateUserStatesInQueue("remove") });
+
 	const updateUserStatesInQueue = (status) =>{
 		if ( status=='add' ){
 			props.socket.emit('addToQueue', {dummy: "dummy"});
